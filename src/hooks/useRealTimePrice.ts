136c6e7b9ab8a6cb6f79fea7ef687d6939712085
WolfieLeader <koren2318@gmail.com>
2022-11-22 14:12:39 +0200

    A little change before adding css framework

--- src/hooks/useRealTimePrice.ts
+++ src/hooks/useRealTimePrice.ts
@@ -1,20 +1,12 @@
 import { useSocket } from "./useSocket";
+import { IMiniTicker } from "../interfaces/binance";
 
-interface IMiniTicker {
-  e: string; // Event type
-  E: number; // Event time
-  s: string; // Symbol
-  c: string; // Close price
-  o: string; // Open price
-  h: string; // High price
-  l: string; // Low price
-  v: string; // Total traded base asset volume
-  q: string; // Total traded quote asset volume
-}
+export const coins = ["BTC", "ETH", "BNB", "ADA", "SOL", "DOT", "LINK", "MATIC", "XRP"];
 
 export const useRealTimePrice = () => {
-  const coins = ["btc", "eth", "bnb", "ada", "dot", "link", "link", "xrp", "sol"];
-  const wsURL = "wss://stream.binance.com:9443/ws/" + coins.join("usdt@miniTicker/");
+  const baseURL = "wss://stream.binance.com:9443/ws/";
+  const streams = coins.map((coin) => `${coin.toLowerCase()}usdt@miniTicker`).join("/");
+  const wsURL = baseURL + streams;
   const { lastMessage, isConnected } = useSocket(wsURL);
   if (isConnected && lastMessage !== null) {
     const { s, c } = lastMessage as IMiniTicker;
---
+++ src/interfaces/binance.d.ts
@@ -0,0 +1,11 @@
+export interface IMiniTicker {
+  e: string; // Event type
+  E: number; // Event time
+  s: string; // Symbol
+  c: string; // Close price
+  o: string; // Open price
+  h: string; // High price
+  l: string; // Low price
+  v: string; // Total traded base asset volume
+  q: string; // Total traded quote asset volume
+}
