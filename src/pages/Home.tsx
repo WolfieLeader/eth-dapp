8305ee290eb763f79c7d9889d9d67b915d3e4fde
WolfieLeader <koren2318@gmail.com>
2022-11-22 15:28:40 +0200

    Tailwind added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>;
 };
 
 export default Home;
